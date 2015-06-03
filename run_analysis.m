f978ac4a37172b525eeddd4982670ea20822c029
Yuan Zhao <yuanzh90@gmail.com>
2015-06-02 19:38:04 -0700

    Integrated plot_data into run_analysis

--- run_analysis.m
+++ run_analysis.m
@@ -30,22 +30,17 @@ fprintf('Processing %d frames.\n', imN);
 %
 %% Generate masks and cell trajectories
 
-tic 
-
-% % Input the number of traps per image
-% colN = input('Input # of traps: ');
-colN = 7;
-
+tic
 % Input the number of fluorescent channels to analyze
 fluN = input('Input # of fluorescent channels: ');
 
 parfor i = 1:posn
     curr_pos = pos{i};
     fprintf('Generating mask and trajectories for position xy%d.\n', str2num(curr_pos));
-    mask_traj(curr_pos,imN,colN,fluN)
+    mask_traj(curr_pos,imN,fluN)
 end
-
 toc
+
 %
 %% Visualization
 
@@ -53,9 +48,41 @@ toc
 flu_array = input('Fluorescent channels to plot [1 2 ...]: ');
 
 % Input fluorescent channel labels
-%label_array = input('Fluorescent channel labels {GFP,irFP,etc.}'); 
+%label_array = input('Fluorescent channel labels {'GFP','irFP',etc.}');
 label_array = {'GFP','mCherry'};
 
-plot_data(colN,pos,flu_array,label_array)
+
+% The following code is a wrapper for the individual plotting scripts based on it. It loads every position's trajectories into all_traj and the manually curated lifespan data into all_lifespan.
+
+% Array for storing all trajectory data across all cells
+all_traj = cell(1,numel(pos));
+% Array for storing manually curated lifespan data
+all_lifespan = cell(1,numel(pos));
+
+% For every position...
+for i = 1:numel(pos)
+    curr_pos = pos{i};
+
+    % Horizontally concatenate traj matrices for every position, forming a super array with dimensions:
+    % # of frames x # of cells/traps (from all positions) x # of fluorescent channel
+    traj_file = ['xy',curr_pos,'/xy',curr_pos,'_traj.mat'];
+    load(traj_file);
+    all_traj{i} = traj;
+
+    % Import manually curated lifespan data for each cell in each position
+    % Add lifespan data to all_lifespan cell array (1 x num pos), where each position's lifespan data is an array with dim:
+    % # of cells rows x 3 cols (cell #, lifespan start frame, lifespan end frame)
+    lifespan_file = csvread(['xy',curr_pos,'/xy',curr_pos,'_lifespan.txt']);
+    all_lifespan{i} = lifespan_file;
+end
+
+% Run plotting scripts here
+
+% Individual subplots for every trajectory
+gridcol = 6; %indicate number of columns of subplots
+
+%data_single(pos,gridcol,all_traj,all_lifespan,flu_array,label_array)
+data_multi(pos,gridcol,all_traj,all_lifespan,flu_array,label_array)
+
 
 %
