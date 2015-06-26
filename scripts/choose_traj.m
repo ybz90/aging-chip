39af2dd08440785db061b0287a677148504e27c6
ybz90 <yuanzh90@gmail.com>
2015-06-26 08:59:35 -0700

    Fixed minor bug, colors switched

--- scripts/choose_traj.m
+++ scripts/choose_traj.m
@@ -156,10 +156,10 @@ function choose_traj(pos_str,gridcol,all_traj,all_lifespan,flu_array,label_array
                 ylabel(ax(2),label_array(2));
                 title(cell_title);
 
-                set(ax,{'ycolor'},{green;crimson})
+                set(ax,{'ycolor'},{crimson;green})
 
-                set(h2, 'color',green, 'linewidth',1.5)
                 set(h1, 'color',crimson, 'linewidth',1.5)
+                set(h2, 'color',green, 'linewidth',1.5)
 
                 hold on
                 axes(ax(1));
